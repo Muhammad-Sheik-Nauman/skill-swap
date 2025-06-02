c0314d55579b6a3512cb39fd4e78ec8f44034036
Salman <salmannajah123@gmail.com>
2025-06-02 18:05:20 +0530

    Add routing with react-router-dom and update App component structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Landing from './pages/Landing'
+import Dashboard from './pages/Dashboard'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Landing />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
