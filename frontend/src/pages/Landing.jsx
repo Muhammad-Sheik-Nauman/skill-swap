9f28ce41c064c01ee8fb3426f8b03b57834e7bd8
Salman <salmannajah123@gmail.com>
2025-06-02 17:34:38 +0530

    Added the components and pages

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '../components/Button'
+
+const Landing = () => {
+  return (
+    <div>
+        <Button>Hi there</Button>
+    </div>
+  )
+}
+
+export default Landing
